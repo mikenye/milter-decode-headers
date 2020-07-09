e039c4aab266ff3bc0367713da4a987624fe4c15
Mike Nye <mike.nye@gmail.com>
2020-07-09 21:07:30 +0800

    work on tests

--- test.sh
+++ test.sh
@@ -2,3 +2,15 @@
 # shellcheck shell=bash
 
 docker build -t milter-decode-headers -f tests/Dockerfile .
+
+docker run \
+    --rm \
+    -d \
+    -e POSTMASTER_EMAIL=postmaster@localdomain \
+    -e POSTFIX_INET_PROTOCOLS=ipv4 \
+    -e POSTFIX_MYDOMAIN=localdomain \
+    -e POSTFIX_REJECT_INVALID_HELO_HOSTNAME=false \
+    -e POSTFIX_REJECT_NON_FQDN_HELO_HOSTNAME=false \
+    -e POSTFIX_REJECT_UNKNOWN_HELO_HOSTNAME=false \
+    -e POSTFIX_SMTPUTF8_ENABLE=true \
+    milter-decode-headers
