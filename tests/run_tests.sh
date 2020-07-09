b539f26c59d0b18a7aaf8e9932dbcfd07aaee8a9
Mike Nye <mike.nye@gmail.com>
2020-07-09 21:27:50 +0800

    work on tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,6 +5,11 @@ echo "Waiting for postfix to become ready..."
 sleep 10
 
 /test_milter_normal.expect 127.0.0.1 25 test nobody@nowhere localdelivery@localdomain
+sleep 10
+
+ls -lah /output/mail
+cat /output mail
+sleep 10
 
 echo "Finished"
 exit 0
