a02ce16e05caadcdbd39083ce0c9d10fbf644ce2
Mike Nye <mike.nye@gmail.com>
2020-07-09 23:17:44 +0800

    work on tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
 
+LIGHTGREEN='\033[1;32m'
+LIGHTRED='\033[1;31m'
+NOCOLOR='\033[0m'
+
 echo "Waiting for postfix to become ready..."
 sleep 10
 
@@ -11,14 +15,29 @@ postfix reload
 sleep 5
 
 # Send test email without subject encoding
-/test_milter_normal.expect 127.0.0.1 25 test nobody@nowhere localdelivery@testserver
+/test_milter_normal.expect 127.0.0.1 25 test nobody@nowhere localdelivery@testserver > /dev/null 2>&1
 sleep 10
+
+cat /output/mail | grep 'Subject: Test email normal' > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+    echo -e "${LIGHTGREEN}PASSED normal email${NOCOLOR}"
+else
+    echo -e "${LIGHTRED}FAILED normal email${NOCOLOR}"
+fi
+rm /output/mail
 
 # Send test email with subject encoding
-/test_milter_encoded.expect 127.0.0.1 25 test nobody@nowhere localdelivery@testserver
+/test_milter_encoded.expect 127.0.0.1 25 test nobody@nowhere localdelivery@testserver > /dev/null 2>&1
 sleep 10
 
-cat /output/mail
+cat /output/mail | grep -e 'X-Decoded-Subject: This is a utf-8 base64 encoded subject' -e 'X-Decoded-Subject-Encoding: utf-8'  > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+    echo -e "${LIGHTGREEN}PASSED encoded email${NOCOLOR}"
+else
+    echo -e "${LIGHTRED}FAILED encoded email${NOCOLOR}"
+fi
+
+cat /output/mail 
 sleep 10
 
 echo "Finished"
