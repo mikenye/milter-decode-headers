29e67e29b16b112cd91aa390b720848b4ccf54ef
Mike Nye <mike.nye@gmail.com>
2020-07-09 22:04:44 +0800

    work on tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,6 +4,13 @@
 echo "Waiting for postfix to become ready..."
 sleep 10
 
+# Update postfix configuration
+echo "smtpd_milters = inet:127.0.0.1:8899" >> /etc/postfix/main.cf && \
+echo "mydestination = testserver" >> /etc/postfix/main.cf && \
+postfix reload
+sleep 5
+
+# Send test email without subject encoding
 /test_milter_normal.expect 127.0.0.1 25 test nobody@nowhere localdelivery@testserver
 sleep 10
 
