9a6ed1a10f49f28bf70fbbe809d679f841e94f02
Mike Nye <mike.nye@gmail.com>
2020-07-09 20:41:26 +0800

    Initial commit

---
+++ decode-headers.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+import syslog
+import Milter
+from email.header import decode_header
+
+class DecodeHeaders(Milter.Base):
+
+    def __init__(self):  # A new instance with each new connection.
+        self.id = Milter.uniqueID()  # Integer incremented with each call.
+
+    def connect(self, IPname, family, hostaddr):
+        self.log("connect from %s at %s" % (IPname, hostaddr))
+        self.headers = list()
+        return Milter.CONTINUE
+
+    def header(self, name, hval):
+        print("----")
+        print(type(name))
+        print(name)
+        print(type(hval))
+        print("hval")
+        return Milter.CONTINUE
+
+def main():
+    # todo: use argparse
+    # todo: better logging to stdout
+    socketname = "inet:8899@127.0.0.1"
+    timeout = 600
+
+    Milter.factory = DecodeHeaders
+    Milter.set_flags(Milter.ADDHDRS)
+
+    syslog.syslog("decodeheaders startup")
+    Milter.runmilter("decodeheaders",socketname,timeout)
+    syslog.syslog("shutdown")
+
+if __name__ == "__main__":
+  main()
