ad7570f157b39aab279823251b4520ed82cc433a
Mike Nye <mike.nye@gmail.com>
2020-07-09 23:32:33 +0800

    Update tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,20 +3,23 @@
 
 LIGHTGREEN='\033[1;32m'
 LIGHTRED='\033[1;31m'
+LIGHTBLUE='\033[1;34m'
 NOCOLOR='\033[0m'
 
 EXITCODE=0
 
-echo "Waiting for postfix to become ready..."
+echo -e "${LIGHTBLUE}Waiting for postfix to become ready...${NOCOLOR}"
 sleep 10
 
 # Update postfix configuration
+echo -e "${LIGHTBLUE}Updating postfix configuration for testing...${NOCOLOR}"
 echo "smtpd_milters = inet:127.0.0.1:8899" >> /etc/postfix/main.cf && \
 echo "mydestination = testserver" >> /etc/postfix/main.cf && \
 postfix reload
 sleep 5
 
 # Send test email without subject encoding
+echo -e "${LIGHTBLUE}Send test email without subject encoding...${NOCOLOR}"
 /test_milter_normal.expect 127.0.0.1 25 test nobody@nowhere localdelivery@testserver > /dev/null 2>&1
 sleep 10
 
@@ -30,6 +33,7 @@ fi
 rm /output/mail
 
 # Send test email with subject encoding
+echo -e "${LIGHTBLUE}Send test email with subject encoding...${NOCOLOR}"
 /test_milter_encoded.expect 127.0.0.1 25 test nobody@nowhere localdelivery@testserver > /dev/null 2>&1
 sleep 10
 
@@ -43,5 +47,5 @@ fi
 
 sleep 10
 
-echo "Finished"
+echo -e "${LIGHTBLUE}Finished...${NOCOLOR}"
 exit $EXITCODE
