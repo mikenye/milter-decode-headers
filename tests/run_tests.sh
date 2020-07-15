3576f459f44f6225f644ffc36f2f5b4fce242a1c
Mike Nye <mike.nye@gmail.com>
2020-07-15 17:38:00 +0800

    Update test script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -23,8 +23,8 @@ echo -e "${LIGHTBLUE}Send test email without subject encoding...${NOCOLOR}"
 /test_milter_normal.expect 127.0.0.1 25 test nobody@nowhere localdelivery@testserver > /dev/null 2>&1
 sleep 10
 
-cat /output/mail | grep 'Subject: Test email normal' > /dev/null 2>&1
-if [ "$?" -eq "0" ]; then
+CHECK_NOT_ENCODED=(cat /output/mail | grep 'Subject: Test email normal' > /dev/null 2>&1)
+if CHECK_NOT_ENCODED; then
     echo -e "${LIGHTGREEN}PASSED normal email${NOCOLOR}"
 else
     echo -e "${LIGHTRED}FAILED normal email${NOCOLOR}"
@@ -37,8 +37,8 @@ echo -e "${LIGHTBLUE}Send test email with subject encoding...${NOCOLOR}"
 /test_milter_encoded.expect 127.0.0.1 25 test nobody@nowhere localdelivery@testserver > /dev/null 2>&1
 sleep 10
 
-cat /output/mail | grep -e 'X-Decoded-Subject: This is a utf-8 base64 encoded subject' -e 'X-Decoded-Subject-Encoding: utf-8'  > /dev/null 2>&1
-if [ "$?" -eq "0" ]; then
+CHECK_ENCODED=(cat /output/mail | grep -e 'X-Decoded-Subject: This is a utf-8 base64 encoded subject' -e 'X-Decoded-Subject-Encoding: utf-8'  > /dev/null 2>&1)
+if CHECK_ENCODED; then
     echo -e "${LIGHTGREEN}PASSED encoded email${NOCOLOR}"
 else
     echo -e "${LIGHTRED}FAILED encoded email${NOCOLOR}"
